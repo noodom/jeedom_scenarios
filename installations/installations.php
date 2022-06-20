e944fcd8b71ce53d099c983c30dfbdfece7eeea9
noodom <noodom.fr@gmail.com>
2022-06-20 13:33:45 +0200

    Update installations.php

--- installations/installations.php
+++ installations/installations.php
@@ -24,3 +24,46 @@ if (is_object($scenarioElement) && $scenarioElement->getType() == 'code') {
     }
   }
 }
+  
+// add bloc code
+//$scenario->setLog('scenario element list : ' . $scenario->getScenarioElement()[1]);
+$scenarioElementList = $scenario->getScenarioElement();
+$newScenarioElementId = -1;
+if (sizeOf($scenarioElementList) == 1) {
+  // add a second bloc code
+  $newScenarioElementId = $scenarioElement->copy();
+  $scenario->setLog('add a second bloc code (id=' . $newScenarioElementId . ')');
+  $scenarioElementList[] = $newScenarioElementId;
+  $scenario->setScenarioElement($scenarioElementList);
+  $scenario->save();
+}
+else {
+  // get the second bloc code
+  $newScenarioElementId = $scenario->getScenarioElement()[1];
+  $scenario->setLog('get the second bloc code (id=' . $newScenarioElementId . ')');
+}
+if (sizeOf($scenarioElementList) > 1) {
+  // fill second bloc code
+  $scenario->setLog('fill second bloc code (id=' . $newScenarioElementId . ')');
+  $scenarioSubElement = scenarioElement::byId($newScenarioElementId)->getSubElement('code');
+  $scenarioExpression = $scenarioSubElement->getExpression()[0];
+  $expressionContent = file_get_contents('https://raw.githubusercontent.com/noodom/jeedom_scenarios/main/installations/installations.php');
+  $scenario->setLog('expressionContent = ' . $expressionContent);
+  //eval($expressionContent);
+  $scenarioExpression->setExpression($expressionContent);
+  $scenarioExpression->save();
+  // enable second code
+  $scenario->setLog('enable second bloc code (id=' . $newScenarioElementId . ')');
+  $scenarioSubElement->setOptions('enable', 1);  
+  $scenarioSubElement->save();
+  
+  // disable first code
+  $firstScenarioElementId = $scenario->getScenarioElement()[0];
+  $scenario->setLog('disable first bloc code (id=' . $firstScenarioElementId . ')');
+  $scenarioSubElement = scenarioElement::byId($firstScenarioElementId)->getSubElement('code');
+  $scenarioSubElement->setOptions('enable', 0);
+  $scenarioSubElement->save();
+  // launch scenario
+  //$scenario->launch();
+}
+$scenario->save();
