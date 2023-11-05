1754caae6375e9beee19f37a5e4ab77445025095
noodom <noodom.fr@gmail.com>
2023-11-05 20:21:45 +0100

    set designSuffixName value to V1

--- installations/menuInstallationBeta.php
+++ installations/menuInstallationBeta.php
@@ -1,7 +1,7 @@
 $tags = $scenario->getTags();
 
 // tags to get
-$tags['#tagsList#'] = '{"menuName":"menuNavButton","menuDesignWidth":"1280","menuDesignHeight":"1000","designSuffixName":"noodom_","htmldisplayParent":"Design"}';
+$tags['#tagsList#'] = '{"menuName":"menuNavButton","menuDesignWidth":"1280","menuDesignHeight":"1000","designSuffixName":"V1","htmldisplayParent":"Design"}';
 
 // bloc code to get
 $tags['#githubLink#'] = 'https://raw.githubusercontent.com/noodom/jeedom_menus/master/installation/nooMenusAutomaticInstallationBeta.php';
